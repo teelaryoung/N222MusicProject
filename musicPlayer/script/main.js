1e0f70ee1a7bc6cbdfd5e25aa80ae3c4fd9a2ebc
Tyler Young <youngtyj@hotmail.com>
2014-04-03 12:20:14 -0400

    laid out the style for the homepage and set up the audio player plug-in

---
+++ musicPlayer/script/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	var myPlaylist = [
+
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Sample',
+        artist:'Sample',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.png'
+    }
+];
+
+	var description = 'Lorem ipsum dolor blah blah blah blah blah blah blah';
+	$('#player').ttwMusicPlayer(myPlaylist, {
+		autoPlay:false, 
+                description:description,
+                jPlayer:{
+                    swfPath:'assets/Jplayer.swf'}
+	})
+
+})
