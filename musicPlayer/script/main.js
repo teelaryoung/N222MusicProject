93952f317fc0e1fcc52ee072b61065bead11dcd3
Tyler Young <youngtyj@hotmail.com>
2014-05-02 14:25:26 -0400

    Modified the style on the artist page as well as the album page in order to populate it with the data from the json file. Also added the individual results pages for each artist and populated it with bio. Change the javascript file to populate the data onto the pages, needs commenting

--- musicPlayer/script/main.js
+++ musicPlayer/script/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-	var myPlaylist = [
+    var myPlaylist = [
 
     {
-        mp3:'mix/1.mp3',
+        mp3:'music/backseatFreestyle.mp3',
         oga:'mix/1.ogg',
         title:'Sample',
         artist:'Sample',
@@ -17,27 +17,51 @@ $(document).ready(function(){
      $.getJSON("./data.json", function(data){
         console.log(data);
 
+        $("#kendrick").append("<h4>" + data.artists[0] + "</h4>");
+        $("#kenGenre").append("<h6>" + data.genres[1] + "</h6>");
+        $("#kenBio").append("<p>" + data.albums[0].bio + "</p>");
+
+        $("#led").append("<h4>" + data.artists[1] + "</h4>");
+        $("#ledGenre").append("<h6>" + data.genres[0] + "</h6>");
+        $("#ledBio").append("<p>" + data.albums[4].bio + "</p>");
+
+        $("#luke").append("<h4>" + data.artists[2] + "</h4>");
+        $("#lukeGenre").append("<h6>" + data.genres[3] + "</h6>");
+        $("#lukeBio").append("<p>" + data.albums[1].bio + "</p>");
+
+        $("#linkin").append("<h4>" + data.artists[4] + "</h4>");
+        $("#linkinGenre").append("<h6>" + data.genres[4] + "</h6>");
+        $("#linkinBio").append("<p>" + data.albums[3].bio + "</p>");
+
+        $("#skrill").append("<h4>" + data.artists[3] + "</h4>");
+        $("#skrillGenre").append("<h6>" + data.genres[2] + "</h6>");
+        $("#skrillBio").append("<p>" + data.albums[2].bio + "</p>");
+
         for(var i = 0; i < data.artists.length; i++){
-            $("#artLinks").append("<li>" + data.artists[i] + "</li>");
+            $("#artList").append("<li>" + data.artists[i] + "</li>");
         }
 
         for(var i = 0; i < data.genres.length; i++){
-            $("#genLinks").append("<li>" + data.genres[i] + "</li>");
+            $("#genList").append("<li>" + data.genres[i] + "</li>");
         }
 
         for(var i = 0; i < data.albums.length; i++){
-            $("#albLinks").append("<li>" + data.albums[i].title + "</li>");
+            $("#albList").append("<li>" + data.albums[i].title + "</li>");
+        }
+
+        for(var i = 0; i < data.albums.length; i++){
+            $("#kenTracks").append("<li class='grayTrack'>" + data.albums[0].tracks[i].name + "</li>")
         }
 
 
     })
 
-	var description = 'Lorem ipsum dolor blah blah blah blah blah blah blah';
-	$('#player').ttwMusicPlayer(myPlaylist, {
-		autoPlay:false, 
+    var description = 'Lorem ipsum dolor blah blah blah blah blah blah blah';
+    $('#player').ttwMusicPlayer(myPlaylist, {
+        autoPlay:false, 
                 description:description,
                 jPlayer:{
                     swfPath:'assets/Jplayer.swf'}
-	})
+    })
 
 })
